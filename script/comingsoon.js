c5335a3e561e29f6b61164ae65a197b3de038ac2
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2014-12-01 12:40:25 +0700

    Add slider to second page.

--- script/comingsoon.js
+++ script/comingsoon.js
@@ -1,28 +1,86 @@
 
 // Ready action
 var autoswitchSlide = false;
+var sliderTimer;
+var sliderTimerCount = 0;
+var sliderTimerPaused = false;
+
+var pauseElement = '[class^=pure-u-]';
+
+function sliderTimerStart() {
+    if (sliderTimerPaused) {
+        return;
+    }
+
+    // sliderTimerCount++;
+    NProgress.set(sliderTimerCount / 100);
+
+    if (sliderTimerCount > 99) {
+        console.log(NProgress.status);
+        NProgress.set(1.0)
+        NProgress.remove();
+        $.fn.fullpage.moveSlideRight();
+        console.log("switch slide");
+
+        // Restart
+        NProgress.set(0.0);
+        sliderTimerCount = 0;
+        console.log(NProgress.status);
+    }
+    sliderTimerCount++;
+    console.log(sliderTimerCount);
+}
+
+function sliderTimerPause() {
+    sliderTimerPaused = true;
+}
+
+function sliderTimerResume() {
+    sliderTimerPaused = false;
+}
+
 $(document).ready(function() {
+    NProgress.configure({
+        showSpinner: false
+    });
+    $(pauseElement).mouseenter(function () {
+        sliderTimerPause();
+    });
+    $(pauseElement).mouseleave(function () {
+        console.log('leave');
+        sliderTimerResume();
+    });
+
     $('#fullpage').fullpage({
         navigation: true,
         navigationPosition: 'right',
-        /* navigationTooltips: ['firstSlide', 'secondSlide'] */
         slidesNavigation: true,
         slidesNavPosition: 'bottom',
-        loopHorizontal: true,
+
+        easingcss3: 'linear',
+        easing: 'linear',
 
         anchors: ['a-landing', 'a-excerpt', 'a-closing'],
 
-        afterSlideLoad:  function (anchorLink, index, slideAnchor, slideIndex) {
+        afterLoad: function (anchorLink, index) {
             console.log(anchorLink);
-            if (anchorLink == 'a-excerpt' && autoswitchSlide == false) {
-                autoswitchSlide = true;
-                setInterval(function () {
-                    $.fn.fullpage.moveSlideRight();
-                }, 10000);
+            if (anchorLink == 'a-excerpt') {
+                /* sliderTimerResume(); */
+                NProgress.set(0.0);
+                console.log("setInterval");
+                if (!sliderTimer) {
+                    sliderTimer = setInterval(sliderTimerStart, 100);
+                }
+            }
+        },
+        onLeave: function (index, nextIndex, direction) {
+            if (index == 2) {
+                console.log("Leave");
+                clearInterval(sliderTimer);
+                sliderTimer = null;
+                NProgress.set(1.0)
+                NProgress.remove();
             }
         }
     });
-
-    // Hide slide nav arrows
-    /* $('.fp-controlArrow').hide(); */
 });
