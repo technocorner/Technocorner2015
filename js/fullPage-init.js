4a4137ac382bbbc8082ec6c3ef35c0012a59f8d3
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2014-12-01 08:05:19 +0700

    Tidy up js & css files and remove old files.

--- js/fullPage-init.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#main').fullpage({
-
-        'navigation': true,
-        'scrollingSpeed': 600,
-    });
-});
---
+++ script/comingsoon.js
@@ -0,0 +1,25 @@
+
+// Ready action
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        navigation: true,
+        navigationPosition: 'right',
+        /* navigationTooltips: ['firstSlide', 'secondSlide'] */
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        loopHorizontal: true,
+
+        anchors: ['a-landing', 'a-excerpt', 'a-closing'],
+
+        afterSlideLoad:  function (anchorLink, index, slideAnchor, slideIndex) {
+            if (anchorLink == 'excerpt') {
+                setInterval(function () {
+                    $.fn.fullpage.moveSlideRight();
+                }, 10000);
+            }
+        }
+    });
+
+    // Hide slide nav arrows
+    /* $('.fp-controlArrow').hide(); */
+});
