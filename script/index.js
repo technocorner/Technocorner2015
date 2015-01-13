fc0c8ced9ba7a75938baa17a320e73f052d82749
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-01-13 19:28:20 +0700

    Separating navigation script

--- script/index.js
+++ script/index.js
@@ -20,35 +20,6 @@ $(document).ready(function() {
         afterResize: function () {}
     });
 
-    /*
-     * Headbar : Upper (floating) element contains menu
-     */
-    // Headbar menu object, contains it's properties
-    headbar = {
-        // Height of headbar
-        height: 70,
-        // Headbar is it visible?
-        visible: false
-    };
-
-    /* When scrolled, headbar menu transform into fixed mode */
-    $(window).scroll(function(e){
-        // Threshold position to transform headbar menu
-        if ($(this).scrollTop() < 500) {
-            headbar.visible = false;
-            $('header').removeClass('fixed-mode');
-            return;
-        }
-
-        $('header').addClass('fixed-mode');
-
-        // Check it is already visible? If not, make it visible
-        if (headbar.visible == false) {
-            headbar.visible = true;
-            $('header').css('opacity', 0).animate({opacity: 1}, 500);
-        }
-    });
-
     $('.galeri-photobox').photobox('a', {
         time:0,
         thumbs:false,
