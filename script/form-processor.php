9a253d24a4a5d32c52fa67df1e95f8e32bf46fc4
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-01-24 12:51:13 +0700

    Fix lack of slash error

--- script/form-processor.php
+++ script/form-processor.php
@@ -3,7 +3,7 @@
  * File upload facility into server. Should be secure!
  */
 
-define( 'ROOTDIR',  $_SERVER['DOCUMENT_ROOT'] );
+define( 'ROOTDIR',  $_SERVER['DOCUMENT_ROOT'] . '/');
 define( 'PARTY_DATA',  $_SERVER['DOCUMENT_ROOT'] . '/participant/' );
 define( 'SUBEVENT',  'National Seminar' );
 define( 'WEB',  'http://kmteti.ft.ugm.ac.id/technocorner/' );
