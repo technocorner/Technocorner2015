42fc6c1b5af90c9262d95704ac499a22d05f1615
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-02-03 09:47:00 +0700

    Add notification of SDC registration

--- script/index.js
+++ script/index.js
@@ -15,17 +15,24 @@ $(document).ready(function() {
 
     $('.galeri-photobox').photobox('a', {
         time:0,
-        thumbs:false,
+        thumbs:false
+    });
+
+    smoothScroll.init({
+        speed: 500,               // Integer. How fast to complete the scroll in milliseconds
+        easing: 'easeInOutCubic', // Easing pattern to use
+        updateURL: true,          // Boolean. Whether or not to update the URL with the anchor hash on scroll
+        offset: 70               // Integer. How far to offset the scrolling anchor location in pixels
     });
 
     // Load navigator headbar and footer
     headbar.attachTo('nav#headbar');
     $('footer').load('part.footer.html #footer-inner');
 
-    smoothScroll.init({
-        speed: 500,               // Integer. How fast to complete the scroll in milliseconds
-        easing: 'easeInOutCubic', // Easing pattern to use
-        updateURL: true,          // Boolean. Whether or not to update the URL with the anchor hash on scroll
-        offset: 70,               // Integer. How far to offset the scrolling anchor location in pixels
-    });
+    // Show notification
+    alertify.alert("Batas Waktu Pendaftaran",
+                   "Pendaftaran <strong>Software Development Competition (SDC)</strong> diperpanjang hingga 8 Februari 2014.");
+
+    // Hide after 10s
+    setTimeout(alertify.closeAll, 5000);
 });
